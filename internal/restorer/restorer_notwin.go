281fe1434764a98a69c0e0de7b8cd02fef2e0455
Aneesh Nireshwalia <99904+aneesh-n@users.noreply.github.com>
2024-01-28 02:38:23 -0700

    Fix unused param

--- internal/restorer/restorer_notwin.go
+++ internal/restorer/restorer_notwin.go
@@ -6,6 +6,6 @@ package restorer
 import "github.com/restic/restic/internal/restic"
 
 // addFile adds the file to restorer's progress tracker
-func (res *Restorer) addFile(node *restic.Node, size uint64) {
+func (res *Restorer) addFile(_ *restic.Node, size uint64) {
 	res.progress.AddFile(size)
 }
